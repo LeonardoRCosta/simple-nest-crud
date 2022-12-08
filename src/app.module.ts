cd09525daa599e1529f6daa39041f7bb96403e75
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-08 16:58:28 -0300

    feat: add database connection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [UsersModule],
+  imports: [MongooseModule.forRoot(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.fepmgjx.mongodb.net/test`), UsersModule],
   controllers: [],
   providers: [],
 })
